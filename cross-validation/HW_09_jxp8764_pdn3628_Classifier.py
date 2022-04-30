a0f804ad356fbfda02bec4628a25a9c1a8bebe45
Justin <jpalms2021@gmail.com>
2022-04-30 16:43:27 -0400

    write out the stumps

--- cross-validation/HW_09_jxp8764_pdn3628_Classifier.py
+++ cross-validation/HW_09_jxp8764_pdn3628_Classifier.py
@@ -1,5 +1,29 @@
 def classifier(record):
 	answer = 0
+	# Decision Stump Number 1
+	if record[4] <= 4:
+		answer = answer - 1
+	else:
+		answer = answer + 1
+
+	# Decision Stump Number 2
+	if record[1] <= 137:
+		answer = answer - 1
+	else:
+		answer = answer + 1
+
+	# Decision Stump Number 3
+	if record[5] <= 129:
+		answer = answer - 1
+	else:
+		answer = answer + 1
+
+	# Decision Stump Number 4
+	if record[4] <= 8:
+		answer = answer - 1
+	else:
+		answer = answer + 1
+
 	if answer < 0:
 		return -1
 	else:
--- cross-validation/HW_09_jxp8764_pdn3628_Mentor.py
+++ cross-validation/HW_09_jxp8764_pdn3628_Mentor.py
@@ -1,4 +1,5 @@
-
+import random
+import numpy as np
 
 def read_input(fp):
     # TODO: What should the data be rounded to?
@@ -13,13 +14,31 @@ def read_input(fp):
     fp.write('\t\t\t\tdata.append(float(val))\n\n')
 
 
-def create_classifier(fp, n_stumps):
+def create_classifier(fp, data, class_ids, n_stumps):
     fp.write('def classifier(record):\n')
-    fp.write('\tanswer = 0\n')
+    fp.write('\tanswer = 0\n\n')
 
+    attr_range = []
+    for index in range(len(data)):
+        np_arr = np.array(data[index])
+        print(data[index])
+        attr_range.append([np_arr.min(), np_arr.max()])
+
+    # TODO: Determine how many stumps to use
+    stump_count = n_stumps[2]
     # Decision Stumps
+    for stump in range(stump_count):
+        attr_index = random.randint(0, len(data) - 1)
+        [min_val, max_val] = attr_range[attr_index]
+        threshold = random.randint(int(min_val), int(max_val))
+        print(threshold, min_val, max_val)
+        fp.write('\t# Decision Stump Number %s\n' % (stump + 1))
+        fp.write('\tif record[%s] <= %s:\n' % (attr_index, threshold))
+        fp.write('\t\tanswer = answer - 1\n')
+        fp.write('\telse:\n')
+        fp.write('\t\tanswer = answer + 1\n\n')
 
-
+    # TODO: is it < or <= ... the doc shows both
     # Return result
     fp.write('\tif answer < 0:\n')
     fp.write('\t\treturn -1\n')
@@ -35,9 +54,19 @@ def call_classifier(fp):
 
 if __name__ == '__main__':
     n_stumps = [1, 2, 4, 8, 10, 20, 25, 35, 50, 75, 100, 150, 200, 250, 300, 400]
+    data = []
+    class_ids = []
+    with open('Abominable_Data_HW_LABELED_TRAINING_DATA__v770_2215.csv') as dataFile:
+        for _tmp in range(len(dataFile.readline().strip().split(",")) - 2):
+            data.append([])
+        for line in dataFile:
+            attributes = line.strip().split(",")
+            for index in range(len(attributes) - 2):
+                data[index].append(float(attributes[index]))
+            class_ids.append(int(attributes[-1]))
     fp = open('HW_09_jxp8764_pdn3628_Classifier.py', 'w')
 
-    create_classifier(fp, n_stumps)
+    create_classifier(fp, data, class_ids, n_stumps)
     read_input(fp)
     call_classifier(fp)
 
